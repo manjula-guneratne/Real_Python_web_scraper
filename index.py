95f369860798990a346b25ddf1090231a36cf1d3
Manjula Guneratne <uftcodingbootcamp@gmail.com>
2024-02-13 22:22:09 -0500

    V1 completed

--- index.py
+++ index.py
@@ -12,18 +12,18 @@ job_elements = results.find_all("div", class_="card-content")
 
 python_jobs = results.find_all("h2", string=lambda text: "python" in text.lower())
 
-for job_element in python_jobs:
+python_job_element = [h2_element.parent.parent.parent for h2_element in python_jobs]
+
+for job_element in python_job_element:
     title_element = job_element.find("h2", class_="title")
     company_element = job_element.find("h3", class_="company")
     location_element = job_element.find("p", class_="location")
+
+    links = job_element.find_all("a")
+    for link in links:
+        link_url = link["href"]
+
     print(title_element.text.strip())
     print(company_element.text.strip())
     print(location_element.text.strip())
-    print()    
-
-f = open("data.txt", "a")
-f.write(soup.text)
-f.close()
-
-print("data added")
-# print(page.text)
+    print(f"Learn here: {link_url}\n")
